9a76fae2c6112e16a56ca9b294e59c88477caee7
Radha Mastandrea <radha.mastandrea@gmail.com>
2023-04-04 16:33:17 -0700

    Update hash_map_radha.hpp

--- hash_map_radha.hpp
+++ hash_map_radha.hpp
@@ -6,6 +6,9 @@
 
 struct HashMap {
 
+    // Create the atomic domain here
+    upcxx::atomic_domain<int> ad = upcxx::atomic_domain<int>({upcxx::atomic_op::compare_exchange,upcxx::atomic_op::load});
+
     size_t full_table_size;
     size_t size() const noexcept;
 
@@ -17,7 +20,7 @@ struct HashMap {
     upcxx::dist_object<upcxx::global_ptr<int>> *used_g;
 
     HashMap(size_t full_table_size1, size_t local_table_size1, upcxx::dist_object<upcxx::global_ptr<kmer_pair>> &data_g1, upcxx::dist_object<upcxx::global_ptr<int>> &used_g1);
-
+    ~HashMap();    
     // Most important functions: insert and retrieve
     // k-mers from the hash table.
     bool insert(const kmer_pair& kmer);
@@ -50,18 +53,17 @@ bool HashMap::insert(const kmer_pair& kmer) {
     bool success = false;
     uint64_t global_slot = hash % size();
 
-    // Useful atomic domain
-    upcxx::atomic_domain<int> ad({upcxx::atomic_op::fetch_add});
+   // Useful atomic domain
+   //upcxx::atomic_domain<int> ad1({upcxx::atomic_op::fetch_add});
 
     // Get the index of the processor that has the slot for the hash
     int target_proc_index = global_slot / local_size();
 
- 
     // We may need to consider multiple target processors as things get filled
-    for (int next_proc = 0; next_proc < upcxx::rank_n(); next_proc++) {
+    for (int next_proc = 0; next_proc <= upcxx::rank_n(); next_proc++) {
 
         target_proc_index = (target_proc_index + next_proc) % upcxx::rank_n();
-        
+
         // Get the pointer for used for that target processor
         upcxx::global_ptr<int> target_proc_used_pointer = used_g->fetch(target_proc_index).wait();
 
@@ -72,25 +74,20 @@ bool HashMap::insert(const kmer_pair& kmer) {
 
         for (int probe = 0; (probe + local_slot) < local_size(); probe++) {
 
-
             // Iterate through used of the target processor
             int is_slot_full = ad.fetch_add(target_proc_used_pointer+local_slot+probe, 1, std::memory_order_relaxed).wait();
-
+            
             if (is_slot_full == 0) {
 
                     // Store the kmer
-                     upcxx::global_ptr<kmer_pair> target_proc_data_pointer = data_g->fetch(target_proc_index).wait();
-                     upcxx::rput(kmer, target_proc_data_pointer+local_slot+probe).wait();
-
+                    upcxx::global_ptr<kmer_pair> target_proc_data_pointer = data_g->fetch(target_proc_index).wait();
+                    upcxx::rput(kmer, target_proc_data_pointer+local_slot+probe).wait();
                     success = true;
-                    ad.destroy();    
-
                     return success;  
                     
                 }
             }
         }
-
         return success;
     }
 
@@ -102,13 +99,13 @@ bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
     uint64_t global_slot = hash % size();
 
     // Useful atomic domain
-    upcxx::atomic_domain<int> ad({upcxx::atomic_op::load});
+    //upcxx::atomic_domain<int> ad2({upcxx::atomic_op::load});
 
     // Get the index of the processor that has the slot for the hash
     int target_proc_index = global_slot / local_size();
- 
+
     // We may need to consider multiple target processors as things get filled
-    for (int next_proc = 0; next_proc < upcxx::rank_n(); next_proc++) {
+    for (int next_proc = 0; next_proc < upcxx::rank_n() + 1; next_proc++) {
 
         target_proc_index = (target_proc_index + next_proc) % upcxx::rank_n();
 
@@ -120,27 +117,27 @@ bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
         if (next_proc == 0) {local_slot = global_slot % local_size();}
         else {local_slot = 0;}
 
+
         for (int probe = 0; (probe + local_slot) < local_size(); probe++) {
 
             // Iterate through used of the target processor
             int is_slot_full = ad.load(target_proc_used_pointer+local_slot+probe, std::memory_order_relaxed).wait();
-            
+
             if (is_slot_full > 0) {
+                  
                  upcxx::global_ptr<kmer_pair> target_proc_data_pointer = data_g->fetch(target_proc_index).wait();
-                    val_kmer = upcxx::rget(target_proc_data_pointer+local_slot+probe).wait();
+                val_kmer = upcxx::rget(target_proc_data_pointer+local_slot+probe).wait();
  
                 if (val_kmer.kmer == key_kmer) {
                     success = true;
-                    ad.destroy();    
                     return success;      
                 } 
                     
                 }
             }
         }
-        ad.destroy();    
-        return success;
 
+        return success;
     }
 
 
@@ -167,3 +164,8 @@ bool HashMap::request_slot(uint64_t slot) {
 size_t HashMap::size() const noexcept { return full_table_size; }
 
 size_t HashMap::local_size() const noexcept { return local_table_size; }
+
+HashMap::~HashMap() {
+    // Need to destroy the atomic domain when the hashmap is destroyed
+    ad.destroy();
+}
